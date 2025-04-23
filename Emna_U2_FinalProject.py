a2396659af4942ba890b3e5e15a0298a74cb0721
emna-feki10 <emna.feki@thekaustschool.org>
2025-04-23 15:07:41 +0300

    23/4/25

---
+++ Emna_U2_FinalProject.py
@@ -0,0 +1,3 @@
+recyclables = [ "Plastic Bottle", "Paper Bag", "Bottle Caps", "Glass Bottles", "Metal Can"]
+compostables = ["Banana Peels", "Egg Shells", "Coffee Grounds", "Grass Clippings", "Dry Leaves"]
+trash = ["Tissue Paper", "Plastic Bag", "Chip Bag" ]
